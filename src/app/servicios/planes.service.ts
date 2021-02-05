2802c67ae4c0274ab2fe77d8166880ef143f55d8
unknown <jmdnbvsjm@gmail.com>
2021-02-05 02:47:58 -0400

    cambie de puerto al heroku

--- src/app/servicios/planes.service.ts
+++ src/app/servicios/planes.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class PlanesService {
 
- // url2: string = 'https://angular-api-hotel.herokuapp.com/api/planes'
-  url:string = 'http://localhost:8080/planes'
+ url: string = 'https://angular-api-hotel.herokuapp.com/api/planes'
+ // url:string = 'http://localhost:8080/planes'
   private httpHeaders = new HttpHeaders({ 'Content-type': 'application/json' }) //tipo de respuesta
   constructor(private _http:HttpClient) { }
 
